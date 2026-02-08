2708214cb1be1d12ebdcd848a82ac66a1cd7414a
Shamim-Faqer <219502059+Shamim-Faqer@users.noreply.github.com>
2026-02-08 17:44:20 +0600

    search-app-added

--- src/pages/Apps.jsx
+++ src/pages/Apps.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 function Apps() {
   const [apps, setApps] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     fetch("/AppsList.json")
@@ -9,21 +10,37 @@ function Apps() {
       .then((data) => setApps(data));
   }, []);
 
+  const searchApps = apps.filter((app) =>
+    app.title.toLowerCase().includes(search.toLowerCase()),
+  );
+
   return (
     <div>
-      <div className="text-center py-12 mx-12">
-        <h2>Our All Application</h2>
-        <p>
-          Explore All Apps on the Market developed by us.We code for millions
-        </p>
-        <div className="search flex justify-between p-6">
-          <h3 className="search results">132 apps founded</h3>
-          <input type="text" placeholder="Search here..." className="input" />
+      <div className="text-center pb-12 mx-12">
+        <div className="py-3 md:py-6 lg:py-12 my-3 md:my-6 lg:my-12">
+          <h2>Our All Application</h2>
+          <p className="pb-6">
+            Explore All Apps on the Market developed by us.We code for millions
+          </p>
         </div>
+        <hr />
+        <div className="search md:flex justify-between items-center p-4">
+          <h3 className="searchresults text-3xl font-bold p-4">
+            {searchApps.length} Apps founded...
+          </h3>
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            type="text"
+            placeholder="Search here..."
+            className="input"
+          />
+        </div>
+        <hr />
         <div className="cardss py-12">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-            {apps.map((app) => (
-              <div>
+            {searchApps.map((app, index) => (
+              <div key={index}>
                 <div className="card bg-base-100 shadow-sm">
                   <figure className="px-10 pt-10">
                     <img src={app.image} alt="Shoes" className="rounded-xl" />
